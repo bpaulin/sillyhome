b0cacb40ab295de435d01527e4df6a6f5349a2da
Bruno <117103599+bpaulin-oneup@users.noreply.github.com>
2023-11-05 11:11:47 +0100

    clean

--- README.md
+++
@@ -1,3 +0,0 @@
-kubectl  create namespace flux-system
-
-kubectl  -n flux-system create secret generic sops-age --from-file=age.agekey=./age.txt
--- hacks/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-git checkout dev
-git rebase main
-sed -i 's/branch: main/branch: dev/' cluster/base/flux-system/gotk-sync.yaml
-git add cluster/base/flux-system/gotk-sync.yaml
-git commit -m "switch branch"
-git push -u origin dev
-kubectl --kubeconfig=./provision/kubeconfig apply -f cluster/base/flux-system/gotk-sync.yaml
